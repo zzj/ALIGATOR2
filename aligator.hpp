5a921f5857ffdba3b7744054b5be47584ffc0b3a
zzj <zhangzhaojun@gmail.com>
2011-03-21 10:49:54 -0400

    initial project

---
+++ aligator.hpp
@@ -0,0 +1,38 @@
+
+#ifndef _ALIGATOR_H_
+#define _ALIGATOR_H_
+
+
+
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <functional>
+#include <numeric>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <boost/program_options.hpp>
+#include <boost/program_options/parsers.hpp>
+using namespace std;
+using namespace boost::program_options;
+
+#include "pathway_db.hpp"
+#include "common.hpp"
+#endif /* _ALIGATOR_H_ */
+
+
